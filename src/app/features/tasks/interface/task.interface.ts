db07883e9fc8ed5c35ad3a57acc0ff299f974455
sandro12352 <157259489+sandro12352@users.noreply.github.com>
2026-02-01 23:36:55 -0500

    cambio

--- src/app/features/tasks/interface/task.interface.ts
+++ src/app/features/tasks/interface/task.interface.ts
@@ -1,3 +1,11 @@
+export interface Worker {
+  nombres: string;
+  apellidos: string;
+  id_trabajador: number;
+  id_usuario: number;
+}
+
+
 export interface Task {
   id_cliente_tarea: number;
   id_tarea: number;
@@ -5,8 +13,8 @@ export interface Task {
   descripcion: string;
   estado: 'pendiente' | 'subido' | 'aprobado' | 'rechazado'; // Uso de literales para mayor control
   archivos: ArchivoTarea[];
-  formulario_url?:  string;
-  id_trabajador?: number; // El campo que discutimos, opcional o nulo
+  formulario_url?: string;
+  trabajador: Worker; // El campo que discutimos, opcional o nulo
 }
 
 
